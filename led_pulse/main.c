77a665dee57c1df2bb640dd701029d72beec67a1
marquisepiton <pitonmarquise@gmail.com>
2026-04-12 11:02:28 -0400

    Quick push

--- led_pulse/main.c
+++ led_pulse/main.c
@@ -7,15 +7,15 @@
  * To bypass the arduino abstraction, I need to use the standard AVR Toolchain. (avr-libc: https://www.nongnu.org/avr-libc/user-manual/overview.html) 
  * Using avr-libc is the library I use to write the equivalent of a kernel for the microcontroller. 
  */
-
-#include <time.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <termios.h>
-#include <unistd.h>
-#include <string.h>
-
+// Standard C Library Header:
+#include <time.h> // Used for manipulating time and time information
+#include <stdio.h> // Standard I/O For essential functions like printf() and scanf()
+#include <errno.h> // Used for error reporting
+#include <string.h> // String string manipulation
+//  POSIX/Unix-specific Header: 
+#include <termios.h> //  Used fro controlling terminal I/O characteristics.
+#include <unistd.h> // Unix Standard 
+#include <fcntl.h> //File Control
 int main()
 {
 	// Fixed path and flag
@@ -33,3 +33,21 @@ int main()
 	close(serial_port);
 	return 0; 
 }
+
+struct terminos tty;
+
+if(tcgetattr(serial_port, &tty) != 0){
+	printf("Error %i from tcgetattr: %s\n", errono, strerror(errno));
+	return 1;
+}
+
+//set Baud Rate (speed)
+cfsetispeed(&tty, B9600);
+cfsetospeed(&tty, B9600);
+
+tty.c_cflag &= ~PARENB;
+tty.c_cflag &= ~CSTOPB;
+tty.c_cflag &= ~CSIZE;
+tty.c_cflag &= |= CS8;
+
+tcsetattr(serial_port, TCSANOW, &tty);
