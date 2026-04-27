041e95d76ca17e032eeb949d41af116c2465c2b9
marquisepiton <pitonmarquise@gmail.com>
2026-04-26 20:03:29 -0400

    Comment: planning steps for project

--- led_pulse_2_push_button/main.c
+++ led_pulse_2_push_button/main.c
@@ -1,2 +1,14 @@
 // Goal: Turn LED on when I push a button. 
-//
+#include <stdio.h>
+#include <avrlibc.h>
+
+
+int main() 
+{
+
+	//1. Sketch and plan
+	//2. Assign pin value
+	//3. Test program
+	
+	return 0;
+}
